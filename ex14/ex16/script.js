e8126bd7a9f2732d8d4739e4194037ee1e480dd5
André Marques <77750879+Andremarquesgoulart@users.noreply.github.com>
2021-02-25 19:26:26 -0300

    commit ex 14

---
+++ ex14/ex16/script.js
@@ -0,0 +1,27 @@
+function calcular() {
+    var inic = window.document.getElementById('inicio')
+    var fm = window.document.getElementById('fim')
+    var pass = window.document.getElementById('passo')
+    var res = window.document.getElementById('res')
+    var inicio = Number(inic.value)
+    var fim = Number(fm.value)
+    var passo = Number(pass.value)
+    if (inicio == 0) {
+        res.innerHTML = 'Impossível contar'
+    } else if (fim == 0 || fim < inicio) {
+        window.alert('Insira o valor correto!')
+    } else if (passo <= 0) {
+        window.alert('Passo inválido. Considerando PASSO 1.')
+    } else {
+        for(var c = passo; c <= fim; c =+ inicio)
+        c++
+    }
+    res.innerHTML = `Contando ${c}`
+
+    } 
+
+
+
+
+
+    //res.innerHTML = `Inicio = ${inicio}, Fim = ${fim}, Passo = ${passo}. `
