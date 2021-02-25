d3f689a0f12ac5381bbbe67f5efbf830c4fdfe3f
André Marques <77750879+Andremarquesgoulart@users.noreply.github.com>
2021-02-25 17:22:17 -0300

    aula#14
    Laços com for

---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+/*
+var c = 1
+while (c <= 3) {
+    console.log(`Usando while ${c}`)
+    c++
+}
+*/
+
+// USANDO FOR PARA MESMA FINALIDADE
+for(var c = 1; c <= 6; c++) {
+    console.log(`Usando for ${c}`)
+}
