bca77b6f669f6b04f782e28ba35b0043146399c1
André Marques <77750879+Andremarquesgoulart@users.noreply.github.com>
2021-02-25 16:42:39 -0300

    aula#13 while / do while
    Aula 13 sobre estrutura de laços

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,8 +1,25 @@
+// do while
+var c = 1
+do {                         // Executa o bloco primeiro
+    console.log(`Passo ${c}`) 
+    c++
+} while (c < 6)              // Depois faz o teste
+
+/* WHILE
+var c = 1
+while (c < 6) {              // Testa primeiro
+    console.log(`Passo ${c}`)// Executa o bloco depois
+    c++
+}
+*/
+
+/*
 var c = 1
 while (c < 6) {
     console.log('Tudo Bem?')
     c++ // == c = c + 1
 }
+*/
 
 /*
 console.log('Tudo bem?')
