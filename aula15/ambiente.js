4456dcef4b938bc95bccfdc099be088905105341
André Marques <77750879+Andremarquesgoulart@users.noreply.github.com>
2021-02-28 19:22:37 -0300

    commit função

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,11 +1,10 @@
-let num = [5,8,9,2,3]
-num.push(1)
-num.sort()
+num.push(1) // insere um valor no array
+num.sort() // organiza em ordem crescente
 console.log(num)
 console.log(`Nosso vetor é o ${num}`)
 console.log(`Nosso vetor tem ${num.length} elementos.`)
 console.log(`O primeiro valor do vetor é ${num[0]}, ele esta no indice 0 do ARRAY`)
-let pos = num.indexOf(5)
+let pos = num.indexOf(4) // indexOF() busca um valor no array
 if(pos == -1) {
     console.log('O valor não foi encontrado')
 } else {
---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0 ,n2=0) { // Parametros pré-dedinidos ou opcionais
+    return n1 + n2
+}
+
+console.log(soma(2))
